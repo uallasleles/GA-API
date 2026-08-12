8ac4b2f81561b1fc1b8ec7683d95d5206897865a
Uallas Leles <uallasleles@hotmail.com>
2026-08-12 09:03:17 -0300

    Enviando todos os títulos, inclusive desdobrados, com DUPLIC concatenado com PREST.

--- queries/export-risco-zero.sql
+++ queries/export-risco-zero.sql
@@ -23,9 +23,11 @@ SELECT
     , REGEXP_REPLACE(client.CGCENT, '[^0-9]', '')   AS cnpj             -- CNPJ (PJ) ou CPF (PF) do cliente na Receita Federal
     , REGEXP_REPLACE(client.TELENT, '[^0-9]', '')   AS telefone         -- Telefone principal de contato do cliente
     , TO_CHAR(prest.DTEMISSAO, 'DD/MM/YYYY')        AS dtalancto        -- Data de lançamento da compra/título no sistema
-    -- , TRIM(prest.DUPLIC)                            AS numdocumento     -- Número do documento de registro no sistema
-    , TRIM(prest.DUPLIC)||'-'||TRIM(PREST.PREST)    AS numdocumento    -- Número do documento de registro no sistema
+    -- , TRIM(prest.DUPLIC)                         AS numdocumento     -- Número do documento de registro no sistema
+    , TRIM(prest.DUPLIC)||'-'||TRIM(PREST.PREST)    AS numdocumento     -- Número do documento de registro no sistema
     , prest.VALOR                                   AS valor            -- Valor do título (compras parceladas: uma linha por parcela)
+    -- , TO_CHAR(prest.DTDESD , 'DD/MM/YYYY')       AS dtadesdobramento -- Data em que foi efetuado o pagamento do título
+    -- , TO_CHAR(prest.DTPAG , 'DD/MM/YYYY')        AS dtapagamento     -- Data em que foi efetuado o pagamento do título
     , TO_CHAR(prest.DTBAIXA, 'DD/MM/YYYY')          AS dtacompensacao   -- Data em que foi efetuado o pagamento do título
     , TO_CHAR(prest.DTVENC, 'DD/MM/YYYY')           AS dtavencimento    -- Data de início de contagem para vencimento do título
     -- , prest.CODCOB
@@ -36,19 +38,20 @@ FROM
 	INNER JOIN PCFILIAL filial 
 		ON prest.CODFILIAL = filial.CODIGO
 
-WHERE 1=1
+WHERE 1=1 
     AND prest.CODFILIAL = NVL(:CODFILIAL, prest.CODFILIAL)           	-- filtra a(s) filial(is) que farão parte do envio
     AND prest.DTEMISSAO >= TO_DATE(:DTEMISSAO_INICIAL, 'DD/MM/YYYY') 	-- janela de extração (incremental) - ex: última data já exportada
     AND prest.DTEMISSAO <= TO_DATE(:DTEMISSAO_FINAL,   'DD/MM/YYYY')  
     AND prest.CANCELDESD IS NULL                                     	-- exclui títulos com desdobramento cancelado
-    AND prest.CODCOB NOT IN (
-          'DEVT'
-        , 'BNF'
-        , 'CAN'
-        , 'DESD'
+    AND prest.CODCOB NOT IN ( 
+          'DEVT' 
+        , 'BNF' 
+        , 'CAN' 
+        -- , 'DESD' 
     )   -- Exclui devoluções, bonificações, cancelados e desdobrados
-    AND prest.VALOR > 0                               					-- Garante apenas títulos com valor a receber
+    AND prest.VALOR > 0 -- Garante apenas títulos com valor a receber
     -- AND prest.DUPLIC = 1225824
+    -- AND prest.DUPLIC = 779914
     -- AND prest.DTPAG IS NULL                        					-- Apenas títulos não baixados
 
 ORDER BY 
